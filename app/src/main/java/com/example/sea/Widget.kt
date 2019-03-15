ca70a3d958e259485cff27542ee1c04c6946ffed
Kristian Gyene <kristsgy@student.matnat.uio.no>
2019-03-15 13:13:24 +0100

    Fikset "nå"-ikoner og defaults

---
+++ app/src/main/java/com/example/sea/Widget.kt
@@ -0,0 +1,7 @@
+package com.example.sea
+
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import android.widget.TextView
+
+class Widget(var text: String?, var image: String?)
