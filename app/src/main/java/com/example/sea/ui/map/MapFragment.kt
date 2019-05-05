b0f2db1585c80392d55230863ac6d834e35f727a
Ahmed Abbas <aaabbas@student.matnat.uio.no>
2019-05-05 23:36:58 +0200

    Satt opp visuell fremstilling på kartet for både regn og vind

--- app/src/main/java/com/example/sea/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/sea/ui/map/MapFragment.kt
@@ -18,6 +18,12 @@ import com.google.android.gms.tasks.OnSuccessListener
 import com.google.maps.android.data.geojson.GeoJsonLayer
 import kotlinx.android.synthetic.main.fragment_map.*
 import kotlinx.android.synthetic.main.view_pager.*
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.ui.IconGenerator
 
 class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener,
     GoogleMap.OnMyLocationButtonClickListener, OnSuccessListener<Location>, GoogleMap.OnInfoWindowClickListener, MapContract.View {
@@ -26,6 +32,8 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
     private lateinit var harborsLayer: GeoJsonLayer
     private lateinit var presenter: MapPresenter
     private val fileName = "com.example.sea"
+    private var markers = mutableListOf<Marker>()
+    private val colorPrimary = 0xFFbbdaa4
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
         val rootView = inflater.inflate(R.layout.fragment_map, container, false)
@@ -42,9 +50,8 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
 
         fab.setOnClickListener {presenter.onFABClick()}
 
-        fab_waves.setOnClickListener{presenter.onFABClick()}
-        fab_wind.setOnClickListener{presenter.onFABClick()}
-        fab_rain.setOnClickListener{presenter.onFABClick()}
+        fab_wind.setOnClickListener{presenter.onWindClick()}
+        fab_rain.setOnClickListener{presenter.onRainClick()}
         fab_harbor.setOnClickListener{presenter.onHarborButtonClick()}
     }
 
@@ -67,11 +74,19 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
         setUpHarborMarkers()
     }
 
-    override fun onMarkerClick(p0: Marker?) = false
+    override fun onMarkerClick(p0: Marker?) : Boolean {
+        if(marker != null && marker!!.id == p0!!.id) {
+            return false
+        }
+
+        Toast.makeText(activity!!, "Lat: ${p0!!.position!!.latitude}, Long: ${p0.position!!.longitude}", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
     override fun onMyLocationButtonClick() = false
 
     override fun onInfoWindowClick(p0: Marker?) {
-        presenter.onInfoWindowClick(p0!!.position)
+        presenter.onInfoWindowClick(p0)
     }
 
     // kalles hvis fusedLocationClient finner lastlocation
@@ -109,10 +124,6 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
         presenter.setUpHarborMarkers(harborsLayer)
     }
 
-    override fun showCameraAnimation(currentLatLng: LatLng) {
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 8f))
-    }
-
     private fun showMarker(p0: LatLng?, locationName: String) {
         marker = map.addMarker(MarkerOptions().position(p0!!).title(locationName))
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(p0.latitude, p0.longitude), 8f), 2000, null)
@@ -122,8 +133,14 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
         map.setInfoWindowAdapter(presenter.createInfoWindowAdapter(p0!!, locationName))
     }
 
+    override fun getMarker() = marker
+
     override fun setTitle(text: String) {
         activity!!.toolbar.title = text
+    }
+
+    override fun showCameraAnimation(currentLatLng: LatLng) {
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 8f))
     }
 
     override fun changeTab() {
@@ -131,15 +148,42 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
         tab.getTabAt(0)!!.select()
     }
 
+    override fun hideMarkers() {
+        if(markers.isNotEmpty()) {
+            for (marker in markers) {
+                marker.remove()
+            }
+
+            markers.clear()
+        }
+    }
+
+    override fun showMarkers(data: String, lat: Double, long: Double, type : String) {
+//        val markerOption =  MarkerOptions().title(data).position(LatLng(lat, long))
+//        if(type == "rain") {
+//            markerOption.icon(BitmapDescriptorFactory.fromBitmap(presenter.createRainMarker()))
+//        }
+//        else {
+//            markerOption.icon(BitmapDescriptorFactory.fromBitmap(presenter.createWindMarker()))
+//        }
+
+        val iconFactory = IconGenerator(activity!!)
+        iconFactory.setTextAppearance(R.style.markerTextStyle)
+        iconFactory.setColor(colorPrimary.toInt())
+        val marker = map.addMarker(MarkerOptions().position(LatLng(lat, long)))
+        marker.setIcon(BitmapDescriptorFactory.fromBitmap(iconFactory.makeIcon(data)))
+
+//        val marker = map.addMarker(markerOption)
+        markers.add(marker)
+    }
+
     override fun showButtons() {
-        fab_waves.show()
         fab_wind.show()
         fab_rain.show()
         fab_harbor.show()
     }
 
     override fun showTextView() {
-        fab_waves_text.visibility = View.VISIBLE
         fab_wind_text.visibility = View.VISIBLE
         fab_rain_text.visibility = View.VISIBLE
         fab_harbor_text.visibility = View.VISIBLE
@@ -154,7 +198,6 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
     }
 
     override fun hideTextView() {
-        fab_waves_text.visibility = View.GONE
         fab_wind_text.visibility = View.GONE
         fab_rain_text.visibility = View.GONE
         fab_harbor_text.visibility = View.GONE
@@ -169,9 +212,12 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
     }
 
     override fun hideButtons() {
-        fab_waves.hide()
         fab_wind.hide()
         fab_rain.hide()
         fab_harbor.hide()
+    }
+
+    override fun onFailure(t: Throwable) {
+        Log.d("Error: ", t.toString())
     }
 }
